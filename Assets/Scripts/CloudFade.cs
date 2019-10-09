6d036e419fef04283b6e6ef4a6e9fad1b9c8d5c4
ebb <asoothingebb@tuta.io>
2019-10-09 09:18:08 +0100

    add tentacle

---
+++ Assets/Scripts/CloudFade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CloudFade : MonoBehaviour
+{
+    public Image img;
+    Transform player;
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+        if(player.position.y < 3)
+        {
+            StartCoroutine(Fade());
+        }
+        if(player.position.y < -10)
+        {
+         //   Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+           img.color = new Color(0, 0, 0, i);
+           yield return null;
+        }
+
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+           img.color = new Color(0, 0, 0, i);
+           yield return null;
+        }
+    }
+}
