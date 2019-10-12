cf30201356015546793b482334b0447c18b48f67
ebb <asoothingebb@tuta.io>
2019-10-12 12:15:58 +0100

    change nibblers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour
 
     public float movementSpeed = 100;
     public float horizontalDampening = 5;
+    public float dashTime;
+    public float dashSpeed;
+    public float parryTime;
+    public float maxDist = 5;
+
     Vector2 forceToAdd;
     Camera cam;
 
@@ -24,6 +29,11 @@ public class Player : MonoBehaviour
     {
         //body.AddForce(movementSpeed * forceToAdd.x * Time.deltaTime, 0, movementSpeed * forceToAdd.y * Time.deltaTime);
         body.velocity = Vector3.Lerp(body.velocity, new Vector3(forceToAdd.x * movementSpeed, body.velocity.y, forceToAdd.y * movementSpeed), Time.deltaTime * horizontalDampening);
+        Vector2 temp = new Vector2(transform.position.x, transform.position.z);
+        if (temp.SqrMagnitude() > maxDist*maxDist)
+        {
+            transform.position = new Vector3( temp.normalized.x * maxDist * 0.99f , transform.position.y, temp.normalized.y*maxDist * 0.99f);
+        }
     }
 
     public void move(InputAction.CallbackContext context)
@@ -36,9 +46,25 @@ public class Player : MonoBehaviour
         Debug.Log("dash!\n");
     }
 
+    public IEnumerator Dash()
+    {
+        var time = 0f;
+        while(time < dashTime)
+        {
+            transform.position = new Vector3(forceToAdd.x * dashSpeed * Time.deltaTime, 0, forceToAdd.y * dashSpeed * Time.deltaTime) + transform.position;
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
     public void parry(InputAction.CallbackContext context)
     {
         Debug.Log("parry!\n");
+    }
+
+    public IEnumerator Parry()
+    {
+        yield return null;
     }
 
     public void OnTriggerEnter(Collider other)
