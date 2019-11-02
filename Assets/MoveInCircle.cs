e73fcaefd5ad14b15ce89f3f12cfb1e9bf17a89c
ebb <asoothingebb@tuta.io>
2019-11-02 15:30:40 +0000

    add hitbox and change shader for skate

--- Assets/MoveInCircle.cs
+++ Assets/MoveInCircle.cs
@@ -9,11 +9,20 @@ public class MoveInCircle : MonoBehaviour
     public float speed = 5f;
     public float timeOffset = 0;
     float time;
+    float yRotOffset=0;
     // Update is called once per frame
+
+    private void Start()
+    {
+        if(speed < 0)
+        {
+            yRotOffset = 180;
+        }
+    }
     void Update()
     {
         time = ((Time.time + timeOffset) * speed ) % (2 * Mathf.PI);
         transform.position  = new Vector3(circleSize * Mathf.Sin(time), transform.position.y, circleSize * Mathf.Cos(time));
-        transform.rotation = Quaternion.Euler(0, -90 + time * (360/(Mathf.PI*2)), 0);
+        transform.rotation = Quaternion.Euler(0, -90 + time * (360/(Mathf.PI*2)) + yRotOffset, 0);
     }
 }
