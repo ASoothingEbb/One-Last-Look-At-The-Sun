7eb286381778df09c810fd40143fc60a50e9bea8
ebb <asoothingebb@tuta.io>
2019-11-02 18:52:37 +0000

    add move towards player logic to eel

---
+++ Assets/Scripts/MoveTowardPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardPlayer : MonoBehaviour
+{
+    float activateDistance = 30f;
+    float speed = 2f;
+
+    Transform player;
+    bool activated = false;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activated || (transform.position - player.position).sqrMagnitude < activateDistance * activateDistance)
+        {
+            activated = true;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            transform.LookAt(player);
+        }
+    }
+}
