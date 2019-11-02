33a2bcd448aec2ddd90007911ba8c982e9db42c5
ebb <asoothingebb@tuta.io>
2019-11-02 20:23:19 +0000

    added texture to eel and made it attack player

--- Assets/Scripts/MoveTowardPlayer.cs
+++ Assets/Scripts/MoveTowardPlayer.cs
@@ -6,6 +6,7 @@ public class MoveTowardPlayer : MonoBehaviour
 {
     public float activateDistance = 30f;
     public float speed = 2f;
+    public float horiSpeedMultiplier = 5f;
 
     Transform player;
     bool activated = false;
@@ -20,7 +21,9 @@ public class MoveTowardPlayer : MonoBehaviour
         if(activated || (transform.position - player.position).sqrMagnitude < activateDistance * activateDistance)
         {
             activated = true;
-            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            //transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            Vector3 dist = player.position - transform.position;
+            transform.position = new Vector3(transform.position.x + dist.normalized.x * horiSpeedMultiplier * speed * Time.deltaTime, transform.position.y + dist.normalized.y * speed * Time.deltaTime, transform.position.z + dist.normalized.z * horiSpeedMultiplier * speed * Time.deltaTime);
             transform.LookAt(player);
         }
     }
