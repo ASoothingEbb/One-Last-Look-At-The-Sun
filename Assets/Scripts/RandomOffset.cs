5c1837fa9774469a42674890d15ec96ecc485332
ebb <asoothingebb@tuta.io>
2019-11-25 17:56:22 +0000

    started on murals

---
+++ Assets/Scripts/RandomOffset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomOffset : MonoBehaviour
+{
+    public float maxX = 3f;
+    public float minX = 0f;
+    public float maxZ = 3f;
+    public float minZ = 0f;
+    void Start()
+    {
+        transform.localPosition = transform.localPosition + new Vector3(PitManager.rand(minX, maxX), 0, PitManager.rand(minZ, maxZ));
+    }
+}
--- Assets/Scripts/RotateContinuously.cs
+++ Assets/Scripts/RotateContinuously.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class RotateContinuously : MonoBehaviour
 {
     public float speed = 10;
+    public bool random = false;
+    public float minRandom = 0f;
+    public float maxRandom = 20f;
 
+    private void Start()
+    {
+        if (random)
+        {
+            speed = PitManager.rand(minRandom, maxRandom);
+        }
+    }
     void Update()
     {
         transform.rotation = Quaternion.Euler(0, Time.time * speed, 0);
