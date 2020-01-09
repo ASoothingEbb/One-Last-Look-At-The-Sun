89ad77cb71ba06c9a888ac07964f9f69858df9af
ebb <asoothingebb@tuta.io>
2020-01-09 11:46:29 +0000

    fix bug where clouds at start would pop in after they were in sight of camera

---
+++ Assets/CloudBlanketFixBounds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBlanketFixBounds : MonoBehaviour
+{
+    public float x = 200, y = 200, z = 200;
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.bounds = new Bounds(new Vector3(0, 0, 0), new Vector3(x, y, z));
+    }
+}
