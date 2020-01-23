96676f12d62e161611bb9c41f1f52aec93d9804a
ebb <asoothingebb@tuta.io>
2020-01-23 17:14:29 +0000

    REWORK WHOLE EFFING MOVEMENT OMG

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Experimental.VFX;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.Video;
-using UnityEngine.Experimental.VFX;
 
 public class Player : MonoBehaviour
 {
@@ -13,23 +13,22 @@ public class Player : MonoBehaviour
 
     public float hurtCooldown = 1f;
     float timeSinceLastHurt = 0f;
-    public float movementSpeed = 100;
-    public float horizontalDampening = 5;
-    public float dashTime=0.15f;
-    public float dashSideSpeed=2f;
-    public float dashDownSpeed = 2f;
-    public float parryTime=0.15f;
-    public float parryCooldown = 2f;
-    float timeSinceLastParry = 0  ;
+    public float horizMovementSpeed = 100;
+    public float horizVelDamp = 5;
+    public float vertVelDamp = 2;
+    public float sideSpeedMult = 2f;
+    public float fallSpeedMult = 2f;
     public float parryCamShakeMag = 0.2f;
     public float parryCamShakeTime = 0.075f;
-    public float maxDist = 5;
-    public float maxFallSpeed = 10f;
+    public float maxHorizDist = 5;
+    public float fallSpeed = 10f;
     public float parryEffectIntensity = 1f;
     public Material hurt;
     public Material tapParry;
-    public float slowMultiplier = 0.3f;
+    public float slowSpeedMult = 0.3f;
     bool slowed = false;
+    public float horizVelDampMult = 3;
+    float timeSinceHolding = 0f;
 
     public Image dieScreen;
     VideoPlayer vid;
@@ -37,13 +36,10 @@ public class Player : MonoBehaviour
     int health = 3;
 
     bool holdingParry = false;
-    bool parrying = false;
 
     VisualEffect dashLines;
     public float dashLinesRate = 45f;
     public float dashLinesSpeed = 45f;
-
-    Coroutine lastDash;
 
     Vector2 moveDir;
     Camera cam;
@@ -55,29 +51,45 @@ public class Player : MonoBehaviour
         cam = GetComponentInChildren<Camera>();
         vid = GameObject.FindGameObjectWithTag("vid").GetComponent<VideoPlayer>();
         dashLines = GetComponentInChildren<VisualEffect>();
-
+        tapParry.SetFloat("Vector1_5E361D35", 0);
     }
 
     public void Update()
     {
-        body.velocity = Vector3.Lerp(body.velocity, new Vector3(moveDir.x * movementSpeed, body.velocity.y, moveDir.y * movementSpeed), Time.deltaTime * horizontalDampening);
-        body.velocity = new Vector3(body.velocity.x, Mathf.Max(body.velocity.y, - maxFallSpeed * (slowed ? slowMultiplier : 1)), body.velocity.z);
-        Vector2 temp = new Vector2(transform.position.x, transform.position.z);
-        if (temp.SqrMagnitude() > maxDist*maxDist)
+
+        //CALC NEW VELOCITY
+        //this code is verbose but its easiest to read and has no performance penalties
+        float newX, newY, newZ;
+        if (holdingParry)
         {
-            transform.position = new Vector3( temp.normalized.x * maxDist * 0.99f , transform.position.y, temp.normalized.y*maxDist * 0.99f);
+            newX = Mathf.Lerp(body.velocity.x, moveDir.x * horizMovementSpeed * sideSpeedMult, horizVelDamp * Time.deltaTime * horizVelDampMult);
+            newY = Mathf.Lerp(body.velocity.y, -fallSpeed * fallSpeedMult, vertVelDamp * Time.deltaTime);
+            newZ = Mathf.Lerp(body.velocity.z, moveDir.y * horizMovementSpeed * sideSpeedMult, horizVelDamp * Time.deltaTime * horizVelDampMult);
+        }
+        else if (slowed)
+        {
+            newX = Mathf.Lerp(body.velocity.x, moveDir.x * horizMovementSpeed * slowSpeedMult, horizVelDamp * Time.deltaTime);
+            newY = Mathf.Lerp(body.velocity.y, -fallSpeed * slowSpeedMult, vertVelDamp * Time.deltaTime);
+            newZ = Mathf.Lerp(body.velocity.z, moveDir.y * horizMovementSpeed * slowSpeedMult, horizVelDamp * Time.deltaTime);
+        }
+        else
+        {
+            newX = Mathf.Lerp(body.velocity.x, moveDir.x * horizMovementSpeed, horizVelDamp * Time.deltaTime);
+            newY = Mathf.Lerp(body.velocity.y, -fallSpeed, vertVelDamp * Time.deltaTime);
+            newZ = Mathf.Lerp(body.velocity.z, moveDir.y * horizMovementSpeed, horizVelDamp * Time.deltaTime);
+
+        }
+        body.velocity = new Vector3(newX, newY, newZ);
+
+        //CHECK OUT OF BOUNDS.
+        Vector2 horizPos = new Vector2(transform.position.x, transform.position.z);
+        if (horizPos.sqrMagnitude > maxHorizDist * maxHorizDist)
+        {
+            transform.position = new Vector3(horizPos.normalized.x * maxHorizDist * 0.99f, transform.position.y, horizPos.normalized.y * maxHorizDist * 0.99f);
         }
 
-        if(holdingParry && timeSinceLastParry > parryCooldown)
-        {
-            timeSinceLastParry = 0;
-            StartCoroutine(ExecuteShortParry());
-            lastDash = StartCoroutine(ExecuteShortDash());
-
-        }
-
-        timeSinceLastParry += Time.deltaTime;
         timeSinceLastHurt += Time.deltaTime;
+        timeSinceHolding += Time.deltaTime;
         hurt.SetFloat("Vector1_932E682D", health);
     }
 
@@ -104,19 +116,21 @@ public class Player : MonoBehaviour
 
     public void parry(InputAction.CallbackContext context)
     {
-            if (context.started)
-            {
-                holdingParry = true;
-            }
-            else if (context.canceled)
-            {
-                holdingParry = false;
-            }
+        if (context.started)
+        {
+            holdingParry = true;
+            tapParry.SetFloat("Vector1_5E361D35", parryEffectIntensity);
+        }
+        else if (context.canceled)
+        {
+            holdingParry = false;
+            tapParry.SetFloat("Vector1_5E361D35", 0);
+        }
     }
 
     public IEnumerator FadeMat(Material mat, string property, float start, float stop, float time)
     {
-        for(float i = 0; i < time; i += Time.deltaTime)
+        for (float i = 0; i < time; i += Time.deltaTime)
         {
             mat.SetFloat(property, Mathf.Lerp(start, stop, i / time));
             yield return null;
@@ -126,7 +140,7 @@ public class Player : MonoBehaviour
 
     public IEnumerator Die()
     {
-        StartCoroutine(FadeMat(hurt, "Vector1_932E682D", 0, -1, 1));
+        //StartCoroutine(FadeMat(hurt, "Vector1_932E682D", 0, -1, 1));
         for (float i = 0; i <= 1; i += Time.deltaTime)
         {
             dieScreen.color = new Color(0, 0, 0, i);
@@ -135,44 +149,44 @@ public class Player : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    public IEnumerator ExecuteShortDash()
-    {
-        StartCoroutine(shakeCamera(parryCamShakeMag, parryCamShakeTime));
-        Vector3 dir = new Vector3(moveDir.x, 0, moveDir.y);
+    //public IEnumerator ExecuteShortDash()
+    //{
+    //    StartCoroutine(shakeCamera(parryCamShakeMag, parryCamShakeTime));
+    //    Vector3 dir = new Vector3(moveDir.x, 0, moveDir.y);
 
-        if (dir.sqrMagnitude < 0.1f)
-        {
-            dir.y = -1;
-        }
-        else
-        {
-            dir.y = -maxFallSpeed / dashDownSpeed;
-        }
-        dashLines.SetFloat("rate", dashLinesRate);
-        dashLines.SetVector3("velocity", -dir * dashLinesSpeed);
-        var time = 0f;
-        while(time < dashTime)
-        {
-            body.velocity = new Vector3(dir.normalized.x * dashSideSpeed, dir.normalized.y * dashDownSpeed, dir.normalized.z * dashSideSpeed);
-            time += Time.deltaTime;
-            yield return null;
-        }
-        dashLines.SetFloat("rate", 0);
-        dashLines.SetVector3("velocity", Vector3.zero);
-    } 
+    //    if (dir.sqrMagnitude < 0.1f)
+    //    {
+    //        dir.y = -1;
+    //    }
+    //    else
+    //    {
+    //        dir.y = -fallSpeed / fallSpeedMult;
+    //    }
+    //    dashLines.SetFloat("rate", dashLinesRate);
+    //    dashLines.SetVector3("velocity", -dir * dashLinesSpeed);
+    //    var time = 0f;
+    //    while (time < dashTime)
+    //    {
+    //        body.velocity = new Vector3(dir.normalized.x * sideSpeedMult, dir.normalized.y * fallSpeedMult, dir.normalized.z * sideSpeedMult);
+    //        time += Time.deltaTime;
+    //        yield return null;
+    //    }
+    //    dashLines.SetFloat("rate", 0);
+    //    dashLines.SetVector3("velocity", Vector3.zero);
+    //}
 
-    public IEnumerator ExecuteShortParry()
-    {
-        parrying = true;
-        //Vector1_5E361D35
-        StartCoroutine(FadeMat(tapParry, "Vector1_5E361D35", 0, parryEffectIntensity, parryTime / 2));
-        for (float i = 0; i < parryTime / 2; i += Time.deltaTime)
-            yield return null;
-        StartCoroutine(FadeMat(tapParry, "Vector1_5E361D35", parryEffectIntensity, 0, parryTime / 2));
-        for (float i = 0; i < parryTime / 2; i += Time.deltaTime)
-            yield return null;
-        parrying = false;
-    }
+    //public ienumerator executeshortparry()
+    //{
+    //    parrying = true;
+    //    //vector1_5e361d35
+    //    startcoroutine(fademat(tapparry, "vector1_5e361d35", 0, parryeffectintensity, parrytime / 2));
+    //    for (float i = 0; i < parrytime / 2; i += time.deltatime)
+    //        yield return null;
+    //    startcoroutine(fademat(tapparry, "vector1_5e361d35", parryeffectintensity, 0, parrytime / 2));
+    //    for (float i = 0; i < parrytime / 2; i += time.deltatime)
+    //        yield return null;
+    //    parrying = false;
+    //}
 
     public void OnTriggerEnter(Collider other)
     {
@@ -180,22 +194,11 @@ public class Player : MonoBehaviour
         {
             takeDamage();
         }
-        else if (other.CompareTag("tapParry"))
-        {
-            if (parrying)
-            {
-
-            }
-            else
-            {
-                takeDamage();
-            }
-        }
         else if (other.CompareTag("holdParry"))
         {
             if (holdingParry)
             {
-
+                //play sound
             }
             else
             {
@@ -209,29 +212,22 @@ public class Player : MonoBehaviour
         }
         else if (other.CompareTag("bounce"))
         {
-            if (lastDash != null)
-            {
-                StopCoroutine(lastDash);
-            }
             body.velocity = -body.velocity * 0.5f;
         }
         else if (other.CompareTag("slow"))
         {
-            Debug.Log("enter slow!");
             slowed = true;
         }
     }
 
     public void OnTriggerExit(Collider other)
     {
-        Debug.Log("trigger exit!");
         if (other.CompareTag("vid"))
         {
             vid.Stop();
         }
         else if (other.CompareTag("slow"))
         {
-            Debug.Log("exit slow!");
             slowed = false;
         }
     }
@@ -241,7 +237,7 @@ public class Player : MonoBehaviour
         Transform camTrans = cam.transform;
         var time = 0f;
 
-        while(time < dur)
+        while (time < dur)
         {
             camTrans.localPosition = new Vector3(Random.Range(-1f, 1f) * mag, 0, Random.Range(-1f, 1f) * mag);
             time += Time.deltaTime;
