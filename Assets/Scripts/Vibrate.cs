4abb13bb4085a41a398db77a643d4cf3032cf176
ebb <asoothingebb@tuta.io>
2020-02-09 13:06:56 +0000

    placeholer for end scene

---
+++ Assets/Scripts/Vibrate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrate : MonoBehaviour
+{
+    public float period = 1;
+    public float time = 0;
+    public Vector3 start;
+    public Vector3 end;
+    public bool randomOffset;
+    void Start()
+    {
+        if (randomOffset)
+        {
+            time = PitManager.rand(0, period);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time >= period)
+        {
+            time = 0;
+            Vector3 tmp = start;
+            start = end;
+            end = start;
+        }
+        transform.localPosition = Vector3.Lerp(start, end, time / period);
+        time += Time.deltaTime;
+    }
+}
