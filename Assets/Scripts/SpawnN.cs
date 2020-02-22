e4083263dab72d9a09aef7a6fc38ef11b96d3b3e
ebb <asoothingebb@tuta.io>
2020-02-22 11:32:59 +0000

    made a nicer end background

---
+++ Assets/Scripts/SpawnN.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnN : MonoBehaviour
+{
+    public GameObject prefab;
+    public int n;
+    void Start()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Instantiate(prefab, this.transform);
+        }
+    }
+}
