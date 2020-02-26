6d87113bdb03e67ffe81d42b3969429b194e70f8
ebb <asoothingebb@tuta.io>
2020-02-26 22:02:00 +0000

    change eel look and behaviour, they dont move up now

--- Assets/Scripts/MoveTowardPlayer.cs
+++ Assets/Scripts/MoveTowardPlayer.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class MoveTowardPlayer : MonoBehaviour
 {
-    public float activateDistance = 30f;
     public float speed = 2f;
-    public float horiSpeedMultiplier = 5f;
 
     Transform player;
-    bool activated = false;
+    public bool randomOffset = true;
+    public Vector3 offset;
+    public float randomOffsetMaxDist = 0.25f;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("player").transform;
+
+        if (randomOffset)
+        {
+            offset = new Vector3(PitManager.rand(-randomOffsetMaxDist, randomOffsetMaxDist), 0, PitManager.rand(-randomOffsetMaxDist, randomOffsetMaxDist));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(activated || (transform.position - player.position).sqrMagnitude < activateDistance * activateDistance)
+        if (Mathf.Pow(this.transform.position.x - player.transform.position.x,2) + Mathf.Pow(this.transform.position.z - player.transform.position.z,2) > 0.2f)
         {
-            activated = true;
-            //transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-            Vector3 dist = player.position - transform.position;
-            transform.position = new Vector3(transform.position.x + dist.normalized.x * horiSpeedMultiplier * speed * Time.deltaTime, transform.position.y + dist.normalized.y * speed * Time.deltaTime, transform.position.z + dist.normalized.z * horiSpeedMultiplier * speed * Time.deltaTime);
-            transform.LookAt(player);
+            Vector3 target = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z) + offset;
+
+            Quaternion targetRot = Quaternion.LookRotation(target - this.transform.position);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRot, speed*Time.deltaTime);
+            this.transform.position = Vector3.Lerp(this.transform.position, target, Time.deltaTime * speed);
         }
     }
 }
