3bb74c11643f1580fbe2075398b5dbbb2d7cbc07
ebb <asoothingebb@tuta.io>
2020-02-27 13:20:12 +0000

    change eel behaviour

--- Assets/Scripts/MoveTowardPlayer.cs
+++ Assets/Scripts/MoveTowardPlayer.cs
@@ -10,6 +10,7 @@ public class MoveTowardPlayer : MonoBehaviour
     public bool randomOffset = true;
     public Vector3 offset;
     public float randomOffsetMaxDist = 0.25f;
+    public float noticeDist = 15f;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("player").transform;
@@ -23,13 +24,16 @@ public class MoveTowardPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Pow(this.transform.position.x - player.transform.position.x,2) + Mathf.Pow(this.transform.position.z - player.transform.position.z,2) > 0.2f)
+        if (Mathf.Pow(this.transform.position.x - player.transform.position.x, 2) + Mathf.Pow(this.transform.position.z - player.transform.position.z, 2) > 0.2f)
         {
-            Vector3 target = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z) + offset;
+            Vector3 target = player.transform.position;
 
             Quaternion targetRot = Quaternion.LookRotation(target - this.transform.position);
-            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRot, speed*Time.deltaTime);
-            this.transform.position = Vector3.Lerp(this.transform.position, target, Time.deltaTime * speed);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRot, speed * Time.deltaTime);
+            if (player.transform.position.y - this.transform.position.y < noticeDist)
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, target, Time.deltaTime * speed * 1 / (Mathf.Pow(Mathf.Abs(player.transform.position.y - this.transform.position.y), 0.7f)));
+            }
         }
     }
 }
