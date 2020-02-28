6adc936dc6f097027a720fc87817f236155795de
ebb <asoothingebb@tuta.io>
2020-02-28 11:49:37 +0000

    tried changing eel behaviour again

--- Assets/Scripts/MoveTowardPlayer.cs
+++ Assets/Scripts/MoveTowardPlayer.cs
@@ -24,22 +24,20 @@ public class MoveTowardPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Pow(this.transform.position.x - player.transform.position.x, 2) + Mathf.Pow(this.transform.position.z - player.transform.position.z, 2) > 0.2f)
+        Vector3 target;
+
+        if (player.transform.position.y - this.transform.position.y < noticeDist)
         {
-            Vector3 target;
-
-            if (player.transform.position.y - this.transform.position.y < noticeDist)
-            {
-                target = this.transform.position + new Vector3(0, speed, 0);
-            }
-            else
-            {
-                target = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
-            }
-
-            Quaternion targetRot = Quaternion.LookRotation(target - this.transform.position);
-            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRot, speed * Time.deltaTime);
-            this.transform.position = Vector3.Lerp(this.transform.position, target + offset, Time.deltaTime * speed * 1 / (Mathf.Pow(Mathf.Abs(player.transform.position.y - this.transform.position.y), 0.5f)));
+            target = this.transform.position + new Vector3(0, speed, 0);
         }
+        else
+        {
+            target = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+        }
+
+        Quaternion targetRot = Quaternion.LookRotation(target - this.transform.position);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRot, speed * Time.deltaTime);
+        this.transform.position = Vector3.Lerp(this.transform.position, target + offset, Time.deltaTime * speed * 1 / (Mathf.Pow(Mathf.Abs(player.transform.position.y - this.transform.position.y), 0.5f)));
+        
     }
 }
