5cd2260ea6c73afbc4dec660dd4ad2662d311e23
ebb <asoothingebb@tuta.io>
2020-03-01 10:28:58 +0000

    add sounds to end

--- Assets/Scripts/EndSun.cs
+++ Assets/Scripts/EndSun.cs
@@ -7,6 +7,8 @@ public class EndSun : MonoBehaviour
 {
     public float time = 5f;
     public float endSize = 2700f;
+    public AudioSource fall;
+    public AudioSource boom;
 
     public void OnTriggerEnter(Collider other)
     {
@@ -17,6 +19,8 @@ public class EndSun : MonoBehaviour
 
     IEnumerator Restart()
     {
+        boom.Play();
+        fall.Stop();
         for (float i = 0; i <= time; i += Time.deltaTime)
         {
             this.transform.localScale = Vector3.Lerp(new Vector3(1, 1, 1), new Vector3(endSize, endSize, endSize), i / time);
